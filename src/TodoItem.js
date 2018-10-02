322938165db7bf47871f7753968b450bd6ed5b7f
helloyong <2628578405@qq.com>
2018-10-02 15:42:38 +0800

    add PropTypes in TodoTiem

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   constructor(props) {
@@ -18,6 +19,12 @@ class TodoItem extends Component {
     const { deleteItem, index } = this.props
     deleteItem(index)
   }
+}
+
+TodoItem.propTypes = {
+  item: PropTypes.string,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func
 }
 
 export default TodoItem
